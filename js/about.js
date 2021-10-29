a6fbef1c3e45c19887e6c572acb9899e22bcb35f
aoki0623 <93359730+aoki0623@users.noreply.github.com>
2021-10-29 12:33:12 +0900

    html_aoki_01

---
+++ js/about.js
@@ -0,0 +1,61 @@
+$.scrollify({
+    section : ".box",//1ページスクロールさせたいエリアクラス名
+    scrollbars:"false",//スクロールバー表示・非表示設定
+    interstitialSection : "#header,#footer",//ヘッダーフッターを認識し、1ページスクロールさせず表示されるように設定
+    easing: "swing", // 他にもlinearやeaseOutExpoといったjQueryのeasing指定可能
+      scrollSpeed: 250, // スクロール時の速度
+    
+    //以下、ページネーション設定
+    before:function(i,panels) {
+      var ref = panels[i].attr("data-section-name");
+        $(".pagination .active").removeClass("active");
+        $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+      },
+      afterRender:function() {
+        var pagination = "<ul class=\"pagination\">";
+        var activeClass = "";
+        $(".box").each(function(i) {//1ページスクロールさせたいエリアクラス名を指定
+          activeClass = "";
+          if(i===$.scrollify.currentIndex()) {
+            activeClass = "active";
+          }
+          pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+        });
+        pagination += "</ul>";
+  
+        $("#box1").append(pagination);//はじめのエリアにページネーションを表示
+        $(".pagination a").on("click",$.scrollify.move);
+      }
+  
+    });
+    //ドロップダウンの設定を関数でまとめる
+function mediaQueriesWin(){
+  var width = $(window).width();
+  if(width <= 768) {//横幅が768px以下の場合
+    $(".has-child>a").off('click'); //has-childクラスがついたaタグのonイベントを複数登録を避ける為offにして一旦初期状態へ
+    $(".has-child>a").on('click', function() {//has-childクラスがついたaタグをクリックしたら
+      var parentElem =  $(this).parent();// aタグから見た親要素の<li>を取得し
+      $(parentElem).toggleClass('active');//矢印方向を変えるためのクラス名を付与して
+      $(parentElem).children('ul').stop().slideToggle(500);//liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
+      return false;//リンクの無効化
+    });
+  }else{//横幅が768px以上の場合
+    $(".has-child>a").off('click');//has-childクラスがついたaタグのonイベントをoff(無効)にし
+    $(".has-child>a").removeClass('active');//activeクラスを削除
+    $('.has-child').children('ul').css("display","");//スライドトグルで動作したdisplayも無効化にする
+  }
+}
+
+// ページがリサイズされたら動かしたい場合の記述
+$(window).resize(function() {
+  mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load',function(){
+  mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
+});
+  
+  
+  
+  
